75a21fcc14ed74a45933865336f0f8dcdf4e7f46
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-03-26 15:59:37 +0100

    feat: models created

---
+++ models/post.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+    },
+    followers: {
+        type: String,
+    },
+    following: {
+        type: String,
+    },
+    likes: {
+        type: String,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const toJSONConfig = {
+    transform: (doc,ret,opt) => {//transform es un metodo de mongoose
+           delete ret['password']//ret es un metodo encripta la password para enviarla con mas seguridad
+           return ret
+    }
+}
+
+
+userSchema.set('toJSON', toJSONConfig);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
