be80b9a53a78f47c6c8177a9aebc628ce3a6196a
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-03-26 16:43:04 +0100

    feat: user endpoints defined

--- views/UsersRouter.js
+++ views/UsersRouter.js
@@ -11,34 +11,62 @@ router.post('/registro', UsuariosController.registraUsuario);
 //http://localhost:3000/Usuarios/registro
 
 
+//Registro
+router.post("/registro", async(req, res) => {
+    try {
+        const user = req.body;
+        res.json(await UsersController.createUser(user));
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
+//http://localhost:3000/Users/registro
+
+
+//Login
+router.post("/login", async(req, res) => {
+    try {
+        let correo = req.body.email;
+        let password = req.body.password;
+        res.json(await UsersController.loginUser(user));
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
+//http://localhost:3000/Users/registro
 
 
 
+//Login
+router.post('/login', UsuariosController.loginUsuarios);
+//http://localhost:3000/Usuarios/login
 
 
+//profile
 
-// //Login
-// router.post('/login', UsuariosController.loginUsuarios);
-// //http://localhost:3000/Usuarios/login
-
-
-// //profile
-
-// //Lee Usuario por id
+//Lee Usuario por id
+//http://localhost:3000/Usuarios/leer/id/:id
 // router.get('/leer/id/:id', auth, isAdmin, UsuariosController.traerUsuarioId);
-// //http://localhost:3000/Usuarios/leer/id/:id
+router.get('/leer/id/:id', async(req, res) => {
+    
+}auth, isAdmin, UsuariosController.traerUsuarioId);
 
-// //Actualizar contraseña de Usuario por id
-// router.put('/actualizar/newpassword', auth, UsuariosController.updatePasswordId);
-// //http://localhost:3000/Usuarios/actualizar/newpassword
+//Actualizar contraseña de Usuario por id
+router.put('/actualizar/newpassword', auth, UsuariosController.updatePasswordId);
+//http://localhost:3000/Usuarios/actualizar/newpassword
 
-// //Actualizar datos de Usuario por id
-// router.put('/actualizar/perfilId/:id', UsuariosController.updateProfileId);
-// //http://localhost:3000/Usuarios/actualizar/email/:id
 
-// //Borramos a usuario por id
-// router.delete('/borrar/id/:id', auth, isAdmin, UsuariosController.deleteById);
-// //http://localhost:3000/Usuarios/borrar/id/:id
+//Actualizar datos de Usuario por id
+router.put('/actualizar/perfilId/:id', UsuariosController.updateProfileId);
+//http://localhost:3000/Usuarios/actualizar/email/:id
+
+//Borramos a usuario por id
+router.delete('/borrar/id/:id', auth, isAdmin, UsuariosController.deleteById);
+//http://localhost:3000/Usuarios/borrar/id/:id
 
 
 // //superusuario
