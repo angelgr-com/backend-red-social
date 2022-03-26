00b809c4324d3fc34485d6616e0005bb5d082b8b
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-03-26 16:12:08 +0100

    feat: endpoints defined

--- views/UsersRouter.js
+++ views/UsersRouter.js
@@ -4,6 +4,68 @@ const UsersController = require('../controllers/UsersController');
 
 //CRUD
 
+
+
+//Registro
+router.post('/registro', UsuariosController.registraUsuario);
+//http://localhost:3000/Usuarios/registro
+
+
+
+
+
+
+
+
+// //Login
+// router.post('/login', UsuariosController.loginUsuarios);
+// //http://localhost:3000/Usuarios/login
+
+
+// //profile
+
+// //Lee Usuario por id
+// router.get('/leer/id/:id', auth, isAdmin, UsuariosController.traerUsuarioId);
+// //http://localhost:3000/Usuarios/leer/id/:id
+
+// //Actualizar contraseña de Usuario por id
+// router.put('/actualizar/newpassword', auth, UsuariosController.updatePasswordId);
+// //http://localhost:3000/Usuarios/actualizar/newpassword
+
+// //Actualizar datos de Usuario por id
+// router.put('/actualizar/perfilId/:id', UsuariosController.updateProfileId);
+// //http://localhost:3000/Usuarios/actualizar/email/:id
+
+// //Borramos a usuario por id
+// router.delete('/borrar/id/:id', auth, isAdmin, UsuariosController.deleteById);
+// //http://localhost:3000/Usuarios/borrar/id/:id
+
+
+// //superusuario
+
+// //Subir de nivel a superusuario de Usuario por id
+// router.put('/actualizar/idAdmin', auth, UsuariosController.idAdmin);
+// //http://localhost:3000/Usuarios/actualizar/idAdmin
+
+// //Convertir en Auth de Usuario por id
+// router.put('/actualizar/idAuth', auth, UsuariosController.degradeProfileId);
+// //http://localhost:3000/Usuarios/actualizar/email/idAuth
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //GET - Find all data of all clients
 
 router.get("/", async(req, res) => {
