538698d9fd07ac4c084b6591fda35cd545ea146e
angelgr-com <dev@angelgr.com>
2022-03-29 18:13:31 +0200

    fix: update endpoint urls in UsersRouter.js

--- views/PostsRouter.js
+++ views/PostsRouter.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const PostsController = require('../controllers/PostsController');
 
 // Posts
-router.get("/:_id", PostsController.getPost);
+router.get("/id/:_id", PostsController.getPost);
 router.post("/", PostsController.newPost);
 router.put("/", PostsController.updatePost);
-router.delete("/:_id", PostsController.deletePost);
+router.delete("/id/:_id", PostsController.deletePost);
 
 // Threads
 router.get("/:title", PostsController.getThreadByTitle);
-router.get("/id/:_id", PostsController.getThreadById);
-router.delete("/delete/:title", PostsController.deleteThread);
+router.get("/thread/:_id", PostsController.getThreadById);
+router.delete("/:title", PostsController.deleteThread);
 
 // Likes
 router.get("/likes/:author", PostsController.totalLikesByAuthor);
