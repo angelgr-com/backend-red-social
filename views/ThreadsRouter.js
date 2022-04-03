f2ccd433e31386705bc83b73102f4b01cb33ce5f
angelgr-com <dev@angelgr.com>
2022-04-03 21:17:45 +0200

    fix: add check authentication in thread endpoints

--- views/ThreadsRouter.js
+++ views/ThreadsRouter.js
@@ -4,12 +4,12 @@ const auth = require("../middlewares/auth");
 const isAdmin = require("../middlewares/isAdmin");
 
 // Threads
-router.post("/", ThreadsController.newThread);
-router.get("/", ThreadsController.getAllThreads);
-router.get("/:title", ThreadsController.getThread);
-router.get("/theme/:theme", ThreadsController.getThreadsByTheme);
-router.put("/:title", ThreadsController.editThread);
-router.delete("/:id", ThreadsController.deleteThread);
+router.post("/", auth, ThreadsController.newThread);
+router.get("/", auth, isAdmin, ThreadsController.getAllThreads);
+router.get("/:title", auth, ThreadsController.getThread);
+router.get("/theme/:theme", auth, ThreadsController.getThreadsByTheme);
+router.put("/:title", auth, ThreadsController.editThread);
+router.delete("/:id", auth, ThreadsController.deleteThread);
 // router.post("/", auth, ThreadsController.newThread);
 // router.get("/", auth, isAdmin, ThreadsController.getAllThreads);
 // router.get("/:title", auth, ThreadsController.getThread);
@@ -18,18 +18,18 @@ router.delete("/:id", ThreadsController.deleteThread);
 // router.delete("/:title", auth, isAdmin, ThreadsController.deleteThread);
 
 // Posts
-router.get("/comments/all/:title", ThreadsController.getComments);
-router.put("/comments/new/:title", ThreadsController.newComment);
-router.put("/comments/edit/:id/:title", ThreadsController.editComment);
-router.delete("/comments/delete/:id/:title", ThreadsController.deleteComment)
+router.get("/comments/all/:title", auth, ThreadsController.getComments);
+router.put("/comments/new/:title", auth, ThreadsController.newComment);
+router.put("/comments/edit/:id/:title", auth, ThreadsController.editComment);
+router.delete("/comments/delete/:id/:title", auth, ThreadsController.deleteComment)
 // router.get("/comments/all/:title", auth, ThreadsController.getComments);
 // router.put("/comments/new/:title", auth, ThreadsController.newComment);
 // router.put("/comments/edit/:title", auth, ThreadsController.editComment);
 // router.delete("/comments/delete/:index/:title", auth, ThreadsController.deleteComment)
 
 // Likes
-router.get("/likes/:id/:title/", ThreadsController.newLike);
-router.get("/likes/:index/:title", ThreadsController.likesFromComment);
+router.get("/likes/:id/:title/", auth, ThreadsController.newLike);
+router.get("/likes/:index/:title", auth, ThreadsController.likesFromComment);
 // router.put("/likes/:index/:title", auth, ThreadsController.newLike);
 // router.get("/likes/:index/:title", auth, ThreadsController.likesFromComment);
 // router.get("/likes/:author", ThreadsController.totalLikesByAuthor);
